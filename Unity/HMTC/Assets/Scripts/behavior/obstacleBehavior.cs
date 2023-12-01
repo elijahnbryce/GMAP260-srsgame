29d5435509f9bf3c2bb3a70eb501a0f7656d2869
elijahnbryce <elijahnbryce@gmail.com>
2023-12-01 17:00:17 -0500

    tuned player and obstacles
    applied no friction material to player so they don't stick to terrain
    
    edited obstacle script to allow for static objects that the mouse cannot move

--- Unity/HMTC/Assets/Scripts/behavior/obstacleBehavior.cs
+++ Unity/HMTC/Assets/Scripts/behavior/obstacleBehavior.cs
@@ -10,27 +10,47 @@ public class obstacleBehavior : MonoBehaviour
     //[SerializeField] Transform testPos;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private float checkRadius = 0.2f;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private float clippingCheckRadius = 0.1f;
+
     private Rigidbody2D rb;
     public bool isAwake;
+    private bool wasStatic;
+    public bool doesNotFall;
 
     void Start()
     {
         //handControl = theHand.GetComponent<handController>();
         rb = GetComponent<Rigidbody2D>();
         isAwake = true;
+        wasStatic = (rb.bodyType == RigidbodyType2D.Static);
+
     }
 
     void Update()
     {
-        if (IsGrounded() && isAwake)
+        if (isAwake)
         {
-            rb.transform.position = new Vector3(rb.transform.position.x, rb.transform.position.y + .1f, rb.transform.position.z);
+            if(IsGrounded(groundCheck, clippingCheckRadius, groundLayer))
+            {
+                rb.transform.position = new Vector3(rb.transform.position.x, rb.transform.position.y + .1f, rb.transform.position.z);
+            }
+            if (IsGrounded(groundCheck, groundCheckRadius, groundLayer))
+            {
+                if (wasStatic)
+                {
+                    rb.bodyType = RigidbodyType2D.Static;
+                }
+            }
+            if (isAwake && doesNotFall)
+            {
+                rb.bodyType = RigidbodyType2D.Static;
+            }
         }
     }
 
-    private bool IsGrounded()
+    private bool IsGrounded(Transform t, float r, LayerMask l)
     {
-        return Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+        return Physics2D.OverlapCircle(t.position, r, l);
     }
 }
