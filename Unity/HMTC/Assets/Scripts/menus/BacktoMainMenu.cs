45d526987417090896fa832a37973f058deb2316
Bashira Bint Abu Yahya <bbintabuyahya03@gmail.com>
2023-12-01 17:33:02 -0500

    finished the credits
    added a working "credits" button to main menu. when you click the button, it will take you to the credits page. when you click on the "back to main menu" button on the credits page, it takes you back to the main menu

--- Unity/HMTC/Assets/Scripts/menus/BacktoMainMenu.cs
+++ Unity/HMTC/Assets/Scripts/menus/BacktoMainMenu.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BacktoMainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string sceneToTransitionTo;
 
-    // Update is called once per frame
-    void Update()
+	public void SwitchScene()
     {
-        
+        SceneManager.LoadScene(sceneToTransitionTo);
     }
 }
