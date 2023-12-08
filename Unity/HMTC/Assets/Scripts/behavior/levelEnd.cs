af422b14877282aab6ef5db6c8d09920e1eef846
Bashira Bint Abu Yahya <bbintabuyahya03@gmail.com>
2023-12-08 14:40:29 -0500

    level progression (almost) finalized

--- Unity/HMTC/Assets/Scripts/behavior/levelEnd.cs
+++ Unity/HMTC/Assets/Scripts/behavior/levelEnd.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class levelEnd : MonoBehaviour
 {
@@ -25,7 +26,7 @@ public class levelEnd : MonoBehaviour
         //endCanvas.gameObject.SetActive(true);
         mouseMove.canMove = false;
         handMove.canMove = false;
-        eV.LoadNext();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
 
     }
 }
